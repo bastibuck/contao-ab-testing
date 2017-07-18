2e4869f83c076599e9949653cb5966fd551417bb
PC-BASTI\Basti <mail@bastibuck.de>
2017-07-18 17:46:02 +0200

    add settings to BE view

--- elements/PiwikSetCustomVariable.php
+++ elements/PiwikSetCustomVariable.php
@@ -19,6 +19,13 @@ class PiwikSetCustomVariable extends \ContentElement
     {
       $objTemplate = new \BackendTemplate('be_wildcard');
       $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['CTE']['piwik_setCustomVariable'][0]). ' ###';
+      $strBuildTitle = '<span class="tl_gray" style="font-weight: normal">[';
+      $strBuildTitle .= $this->customVar_index.', ';
+      $strBuildTitle .= '\''.$this->customVar_name.'\', ';
+      $strBuildTitle .= '\''.$this->customVar_value.'\', ';
+      $strBuildTitle .= '\''.$this->customVar_scope.'\'';
+      $strBuildTitle .= ']</span>';
+      $objTemplate->title = $strBuildTitle;
       return $objTemplate->parse();
     }
 
